145bdacb616d2050cec810ccd69e8a4816d9f73f
Mazayw <mazayw@gmail.com>
2022-08-21 16:59:51 +0300

    feat: add authorization window

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Auth from './components/authorization'
 import Footer from './components/footer'
 import Header from './components/header'
 import MainPage from './components/pages/main-page/index'
@@ -7,6 +8,7 @@ import MainPage from './components/pages/main-page/index'
 function App() {
   return (
     <div className='App'>
+      <Auth />
       <Header />
       <MainPage />
       <Footer />
