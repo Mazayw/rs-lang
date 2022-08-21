0a9d2b17b28ea01d70a045faf11327d0e845b1b5
Mazayw <mazayw@gmail.com>
2022-08-21 13:14:22 +0300

    feat: add pages

---
+++ src/components/pages/games/audiocall/index.tsx
@@ -0,0 +1,6 @@
+import styles from './styles.module.scss'
+
+function Audiocall() {
+  return <main className={styles['about-main']}></main>
+}
+export default Audiocall
---
+++ src/components/pages/vocabulary/index.tsx
@@ -0,0 +1,6 @@
+import styles from './styles.module.scss'
+
+function Vocabulary() {
+  return <main className={styles.main}></main>
+}
+export default Vocabulary
