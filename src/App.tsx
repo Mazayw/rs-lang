9a7f50092bc2a3363ed2ea2d265a8a4f7575a0a9
Mazayw <mazayw@gmail.com>
2022-08-23 11:47:51 +0300

    fix: few minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Auth from './components/authorization'
-import Footer from './components/footer'
-import Header from './components/layout'
+
+import Layout from './components/layout'
 import MainPage from './components/pages/main-page/index'
-import { useState } from 'react'
 import Vocabulary from './components/pages/vocabulary/index'
 import Audiocall from './components/pages/games/audiocall/index'
 import Sprint from './components/pages/games/sprint/index'
@@ -15,21 +12,10 @@ import NotFound from './components/pages/notfound/index'
 import GemeDescr from './components/pages/game-desc'
 
 function App() {
-  const [isModalActive, setModalActive] = useState(false)
-  const [isAuthorized, setIsAuthorized] = useState(false)
-  const [authType, setAuthType] = useState('')
-
   return (
     <div className='App'>
-      <Auth
-        active={isModalActive}
-        setActive={setModalActive}
-        setIsAuthorized={setIsAuthorized}
-        authType={authType}
-      />
-
       <Routes>
-        <Route path='/' element={<Header setActive={setModalActive} setAuthType={setAuthType} />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path='vocabulary' element={<Vocabulary />} />
           <Route path='games' element={<GemeDescr />} />
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,38 @@
 import styles from './styles.module.scss'
 import { Outlet } from 'react-router-dom'
 import Footer from '../footer'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from '../navbar'
+import Auth from '../authorization/index'
+import { useState } from 'react'
 
 function Layout() {
+  const [isModalActive, setModalActive] = useState(false)
+  const [isAuthorized, setIsAuthorized] = useState(false)
+  const [authType, setAuthType] = useState('')
   return (
     <>
+      <Auth
+        active={isModalActive}
+        setActive={setModalActive}
+        setIsAuthorized={setIsAuthorized}
+        authType={authType}
+      />
       <header className={styles.header}>
         <div className={styles['header-wrapper']}>
           <img className={styles.logo__img} src='/icons/logo.svg' alt='Logo' />
           <div className={styles.menu}>
             <Navbar />
           </div>
-          <img className={styles.user__img} src='/icons/user.svg' alt='Logo' />
+          <img
+            className={styles.user__img}
+            src='/icons/user.svg'
+            alt='Logo'
+            onClick={() => {
+              setModalActive(true)
+              setAuthType('Регистрация')
+            }}
+          />
         </div>
       </header>
       <Outlet />
