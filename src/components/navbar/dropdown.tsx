d71acda071ef165e37916196d913771395a77155
Mazayw <mazayw@gmail.com>
2022-08-23 16:32:37 +0300

    feat: add dropdown menu

--- src/components/navbar/dropdown.tsx
+++ src/components/navbar/dropdown.tsx
@@ -5,12 +5,15 @@ import styles from './styles.module.scss'
 const Dropdown = ({
   submenus,
   setActive,
+  dropdown,
 }: {
   submenus: Array<IMenuMain>
   setActive: ({ isActive }: { isActive: boolean }) => string
+  dropdown: boolean
 }) => {
   return (
-    <div className={styles.dropdown}>
+    <div className={dropdown ? `${styles.dropdown} ${styles.show}` : styles.dropdown}>
+      <span className={styles.padding}></span>
       {submenus.map((submenu, index) => (
         <NavLink className={setActive} to={submenu.url} key={index}>
           {submenu.title}
--- src/components/navbar/menu-items.tsx
+++ src/components/navbar/menu-items.tsx
@@ -2,24 +2,63 @@ import Dropdown from './dropdown'
 import { IMenu } from '../types/interface'
 import { NavLink } from 'react-router-dom'
 import styles from './styles.module.scss'
+import { useState, useRef, useEffect } from 'react'
 
 export const MenuItems = ({ items }: { items: IMenu }) => {
   const setActive = ({ isActive }: { isActive: boolean }) =>
     isActive ? `${styles.active} ${styles.menu__item}` : styles.menu__item
+
+  const [dropdown, setDropdown] = useState(false)
+
+  const ref = useRef<HTMLLIElement | null>(null)
+  useEffect(() => {
+    const handler = (event: TouchEvent | MouseEvent) => {
+      if (dropdown && ref.current && !ref.current?.contains(event.target as Node)) {
+        setDropdown(false)
+      }
+    }
+    document.addEventListener('mousedown', handler)
+    document.addEventListener('touchstart', handler)
+    return () => {
+      document.removeEventListener('mousedown', handler)
+      document.removeEventListener('touchstart', handler)
+    }
+  }, [dropdown])
+
+  const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true)
+  }
+
+  const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false)
+  }
+
   return (
-    <>
+    <li
+      className={styles['menu__item']}
+      ref={ref}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       {items.submenu ? (
         <>
-          <NavLink role='button' aria-haspopup='menu' to={items.url} className={setActive}>
+          <a
+            role='button'
+            aria-haspopup='menu'
+            className={styles.menu__item}
+            aria-expanded={dropdown ? 'true' : 'false'}
+            onClick={() => setDropdown((prev) => !prev)}
+          >
             {items.title}{' '}
-          </NavLink>
-          <Dropdown submenus={items.submenu} setActive={setActive} />
+          </a>
+          <span className={styles.arrow} />
+          <Dropdown submenus={items.submenu} setActive={setActive} dropdown={dropdown} />
         </>
       ) : (
         <NavLink className={setActive} to={items.url}>
           {items.title}
         </NavLink>
       )}
-    </>
+    </li>
   )
 }
