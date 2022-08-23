7cf31a46c150911138338b0f1c66b1c37ae0e1cc
Mazayw <mazayw@gmail.com>
2022-08-23 19:25:42 +0300

    feat: add layout

--- src/components/pages/main-page/index.tsx
+++ src/components/pages/main-page/index.tsx
@@ -1,6 +1,27 @@
 import styles from './styles.module.scss'
 
 function MainPage() {
-  return <main className={styles.main}></main>
+  return (
+    <div className={styles.main}>
+      <img src='./img/hero-img.png' alt='Girl with books' className={styles['hero-img']} />
+      <div className={styles.info}>
+        <div className={styles['text-block']}>
+          <h1 className={styles.title}>Изучай английский язык вместе с RsLang!</h1>
+          <h3 className={styles.subtitle}>
+            Бесплатный, веселый и эффективный способ выучить язык! Играйте в мини-игры, изучайте
+            новые слова и повторяйте их до закрепления результата!
+          </h3>
+        </div>
+        <div className={styles['buttons-block']}>
+          <button type='button' className={`${styles.button} ${styles.regbtn}`}>
+            регистрация
+          </button>
+          <button type='button' className={`${styles.button} ${styles.authbtn}`}>
+            войти
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default MainPage
