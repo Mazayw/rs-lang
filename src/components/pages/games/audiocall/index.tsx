6f50fb3dc2bdd1aef07a5c673db5cb93d14ee536
Mazayw <mazayw@gmail.com>
2022-08-29 19:06:32 +0300

    fix: state chsanger

--- src/components/pages/games/audiocall/index.tsx
+++ src/components/pages/games/audiocall/index.tsx
@@ -1,12 +1,16 @@
 import Description from './description'
 import styles from './styles.module.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { IWord } from '../../../types/interface'
 import AudioGameMain from './game/index'
 
 function Audiocall() {
   const [gameState, setGameState] = useState(0)
   const words: IWord[] = []
+
+  useEffect(() => {
+    setGameState(0)
+  }, [])
 
   return (
     <div className={styles['about-main']}>
