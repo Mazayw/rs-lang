332c2aa19e91e86394224fc4da8411cd0c5201f3
Mazayw <mazayw@gmail.com>
2023-02-13 22:45:33 +0200

    refactor: create icons for the next and prev pages as a components

--- src/pages/vocabulary/WordButton.tsx
+++ src/pages/vocabulary/WordButton.tsx
@@ -9,82 +9,73 @@ import { VOCABULARY_SETTINGS } from '../../settings'
 import StarIcon from '../../assets/icons/starIcon'
 import useUpdateWord from '../../hooks/useUpdateWord'
 
-const WordButton = observer(
-  ({
-    word,
-    index,
-  }: {
-    word: IWord
-    index: number
-  }) => {
-    const { vocabulary, store } = useContext(Context)
-    /* const { isHardWord, toggleWordDifficulty } = useHardWord(word)
+const WordButton = observer(({ word, index }: { word: IWord; index: number }) => {
+  const { vocabulary, store } = useContext(Context)
+  /* const { isHardWord, toggleWordDifficulty } = useHardWord(word)
     const { isStudiedWord, toggleWordLearned } = useLearnedWords(word)*/
-    const { isHardWord, isStudiedWord, toggleWordDifficulty, toggleWordLearned, newWord } =
-      useUpdateWord(word)
+  const { isHardWord, isStudiedWord, toggleWordDifficulty, toggleWordLearned } = useUpdateWord(word)
 
-    const { getWordsData } = useLoadWords(vocabulary, store)
+  const { getWordsData } = useLoadWords(vocabulary, store)
 
-    const onClickHardWord = async () => {
-      await toggleWordDifficulty()
-      const wordsData = await getWordsData()
-      vocabulary.setWords(wordsData)
-    }
+  const onClickHardWord = async () => {
+    await toggleWordDifficulty()
+    const wordsData = await getWordsData()
+    vocabulary.setWords(wordsData)
+  }
 
-    const onClickLearnedWord = async () => {
-      await toggleWordLearned()
-      getWordsData().then((data) => vocabulary.setWords(data))
-    }
+  const onClickLearnedWord = async () => {
+    await toggleWordLearned()
+    getWordsData().then((data) => vocabulary.setWords(data))
+  }
 
-    useEffect(() => {
-      console.log('change array')
-    }, [vocabulary.words])
+  useEffect(() => {
+    console.log('change array')
+  }, [vocabulary.words])
 
-    return (
-      <li className={styles['word-button-list']}>
-        <button
-          className={`${styles['word-button']} ${
-            index === vocabulary.selectedWordIndex && styles['word-button_active']
+  return (
+    <li className={styles['word-button-list']}>
+      <button
+        className={`${styles['word-button']} ${
+          index === vocabulary.selectedWordIndex && styles['word-button_active']
+        }`}
+        onClick={() => {
+          vocabulary.setSelectedWordIndex(index)
+        }}
+      >
+        <span className={styles['word-button-container']}>
+          <span className={styles['word-button__word']}>{word.word}</span>
+          <span className={styles['word-button__translate']}>{word.wordTranslate}</span>
+        </span>
+        <span
+          className={`${styles['word-button-container-icon']} ${
+            store.isAuth ? styles['word-button-container-icon_active'] : ''
           }`}
-          onClick={() => {
-            vocabulary.setSelectedWordIndex(index)
-          }}
         >
-          <span className={styles['word-button-container']}>
-            <span className={styles['word-button__word']}>{word.word}</span>
-            <span className={styles['word-button__translate']}>{word.wordTranslate}</span>
-          </span>
-          <span
-            className={`${styles['word-button-container-icon']} ${
-              store.isAuth ? styles['word-button-container-icon_active'] : ''
-            }`}
-          >
-            <button className={styles['words-image-auth__button']} onClick={onClickLearnedWord}>
-              <CheckIcon
-                color={
-                  isStudiedWord
-                    ? VOCABULARY_SETTINGS.ACTIVE_ICON_COLOR
-                    : VOCABULARY_SETTINGS.DISABLED_ICON_COLOR
-                }
-                width={'19'}
-                height={'19'}
-              />
-            </button>
-            <button onClick={onClickHardWord} className={styles['words-image-auth__button']}>
-              <StarIcon
-                color={
-                  isHardWord
-                    ? VOCABULARY_SETTINGS.ACTIVE_ICON_COLOR
-                    : VOCABULARY_SETTINGS.DISABLED_ICON_COLOR
-                }
-                width={'19'}
-                height={'19'}
-              />
-            </button>
-          </span>
-        </button>
-      </li>
-    )
-  },
-)
+          <button className={styles['words-image-auth__button']} onClick={onClickLearnedWord}>
+            <CheckIcon
+              color={
+                isStudiedWord
+                  ? VOCABULARY_SETTINGS.ACTIVE_ICON_COLOR
+                  : VOCABULARY_SETTINGS.DISABLED_ICON_COLOR
+              }
+              width={'19'}
+              height={'19'}
+            />
+          </button>
+          <button onClick={onClickHardWord} className={styles['words-image-auth__button']}>
+            <StarIcon
+              color={
+                isHardWord
+                  ? VOCABULARY_SETTINGS.ACTIVE_ICON_COLOR
+                  : VOCABULARY_SETTINGS.DISABLED_ICON_COLOR
+              }
+              width={'19'}
+              height={'19'}
+            />
+          </button>
+        </span>
+      </button>
+    </li>
+  )
+})
 export default WordButton
